550097d91edf07d3caaae17a4f68a1cde57e0562
Ishan Wijesingha <ishan@blinq.me>
2023-07-11 17:45:15 +1000

    added starter files

---
+++ pages/PhoneFrame.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties, ReactNode } from "react";
+
+import styles from "../styles/PhoneFrame.module.css";
+
+interface PhoneFrameProps {
+  children?: ReactNode;
+  insideScreenStyles?: CSSProperties;
+}
+
+export const PhoneFrame: React.FC<PhoneFrameProps> = (props) => {
+  return (
+    <div className={styles["iphone-14"]}>
+      <div className={styles["iphone-14-content"]}>
+        <div className={styles["notch"]}>
+          <div className={styles["camera"]}></div>
+        </div>
+        <div className={styles["sleep"]}></div>
+        <div className={styles["volume"]}></div>
+        <div className={styles["overflow"]}>
+          <div className={styles["shadow shadow--tr"]}></div>
+          <div className={styles["shadow shadow--tl"]}></div>
+          <div className={styles["shadow shadow--br"]}></div>
+          <div className={styles["shadow shadow--bl"]}></div>
+        </div>
+        <div className={styles["screen"]}>
+          {/* inside the phone frame */}
+          <div style={props.insideScreenStyles}>{props.children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhoneFrame;
